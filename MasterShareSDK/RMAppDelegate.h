1b6ac93530b52178f83b0df1e780c811325cdddb
Zarph <ramiro@leapcommerce.com>
2013-04-18 20:25:54 -0300

    Initial Commit

---
+++ MasterShareSDK/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  MasterShareSDK
+//
+//  Created by Ramiro Guerrero on 18/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MasterShareSDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MasterShareSDK
+//
+//  Created by Ramiro Guerrero on 18/04/13.
+//  Copyright (c) 2013 Ramiro Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
