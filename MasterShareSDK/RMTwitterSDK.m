b7971663a15b562a9518c1ba59f76626e1a5285b
msg-ios <msg.maruko@gmail.com>
2013-04-26 19:17:54 -0300

    commit

--- MasterShareSDK/RMTwitterSDK.m
+++ MasterShareSDK/RMTwitterSDK.m
@@ -8,12 +8,6 @@
 
 #import "RMTwitterSDK.h"
 
-static NSString * const kOAuth1BaseURLString = @"https://api.twitter.com/1.1/";
-static NSString * const kConsumerKeyString = @"";//FILL IN WITH YOUR OWN CONSUMER KEY
-static NSString * const kConsumerSecretString = @"";//FILL IN WITH YOUR OWN CONSUMER SECRET
-static NSString * const kTokenString = @"";//FILL IN WITH YOUR OWN TOKEN
-static NSString * const kTokenSecretString = @"";//FILL IN WITH YOUR OWN TOKEN SECRET
-
 @interface RMTwitterSDK()
 @property (nonatomic) ACAccountStore *accountStore;
 @end
